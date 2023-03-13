0b632adca012dac36d33fe1dc257a7a29d7d7051
purushothamrepo <purushothammegavath99@gmail.com>
2023-03-14 03:54:04 +0530

    initials

---
+++ app.js
@@ -0,0 +1,48 @@
+const express= require('express')
+const request=require('request')
+//Syntax of express
+const app= express()
+const dotenv=require('dotenv')
+dotenv.config()
+app.set("view engine", "ejs")
+app.use('/public', express.static('public'))
+
+app.get("/", (req, res)=>{
+    res.render("home")
+})
+
+app.get("/result", (req, res)=>{
+    console.log(req.query.movieName)
+    
+    const url= `http://www.omdbapi.com/?apikey=98f4eda1&s=${req.query.movieName}`
+    request(url, function (error, response, body){
+        if(!error && response.statusCode==200){
+            const data= JSON.parse(body)
+            
+            res.render("result", {movieData: data})
+        }else{
+            res.send("Uh oh error")
+        }
+    })
+})
+app.get("/result/:id", (req, res)=>{
+    
+    console.log(process.env.API_KEY)
+    const url= `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${req.params.id}`
+    request(url, function (error, response, body){
+        if(!error && response.statusCode==200){
+            const data= JSON.parse(body)
+          
+            res.render("AboutMovie", {movieData: data})
+        }else{
+            res.send("Uh oh error")
+        }
+    })
+})
+app.get("*", (req, res)=>{
+    res.send("Go back! Illegal response")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server has started at port ${process.env.PORT}`)
+})
